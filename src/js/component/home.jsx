bea349c7e4104aabdb1ce6e68c08c3eeba2f607c
Noé Ibáñez Torres <132618491+Noesersi@users.noreply.github.com>
2023-08-15 11:10:31 +0000

    first commit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,60 @@
-import React from "react";
+import React, { useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
+// Create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+  const [inputValue, setInputValue] = useState("");
+  const [todos, setTodos] = useState([]);
+  const [color, setColor] = useState("black")
+
+  const handleInputChange = (e) => {
+    setInputValue(e.target.value);
+  };
+
+  const handleAddTodo = () => {
+    if (inputValue !== "") {
+      setTodos([...todos, inputValue]);
+      setColor(randomColor());
+      alert('Has añadido una nueva tarea ', )
+      setInputValue("");
+    }
+  };
+  const handleDeleteTodo = (index) => {
+    const newTodos = todos.filter((value,i) => i !== index);
+    alert('acabas de borrar una tarea!! ten cuidado no vayas a dejarte algo por hacer!!!')
+    setTodos(newTodos);
+  };
+  function randomColor(){
+    const colors = [ 'green', 'blue', 'yellow', 'red', 'purple', 'cian', 'grey','orange']
+    const randomColor = Math.floor(Math.random()*colors.length);
+    return colors[randomColor];
+  }
+
+  return (
+    <div className="container col-12 col-md-6">
+      <h1 style={{color: color}}>My ToDo's</h1>
+      <ul>
+        <li>
+          <input
+            type="text"
+            value={inputValue}
+            placeholder="What do you need to do baby??"
+            onChange={handleInputChange}
+          />
+          <i className="fa-solid fa-check icon" onClick={handleAddTodo}></i>
+        </li>
+        {todos.map((todo, index) => (
+          <li key={index}>
+            {todo}
+            <i
+              className="fas fa-trash-alt delete-icon"
+              onClick={() => handleDeleteTodo(index)}
+            ></i>
+          </li>
+        ))}
+        <div style={{color: color}}>{todos.length} tasks</div>
+      </ul>
+    </div>
+  );
 };
 
 export default Home;
